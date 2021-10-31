8f93c306a5c6a47574ccbeaac4f65de59c73baa1
sarangkulkarni99 <sarang.kulkarni99@gmail.com>
2021-10-31 21:14:15 +0530

    Team Page

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,9 +1,87 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Team.css";
 
 const Team = () => {
   return (
     <>
-      <h1>Team Page</h1>
+      <div class="container">
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div class="col">
+      <div class="card">
+        <div class="blue-card-background">
+          <img src="./images/profile.jpg" class="card-image"/>
+        </div>
+        <div class="blue-card-info">
+          <h3>Lorem Ipsum</h3>
+          <br/>
+          <h5>Lorem ipsum dolor sit</h5>
+          <p>Lorem ipsum | Lorem Ipsum | Lorem Ipsum | Lorem Ipsum</p>
+        </div>
+        <div class="card-social-icons">
+          <FontAwesomeIcon icon={['fab', 'apple']}/>
+          <i class="fab"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-facebook-square"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="red-card-background">
+          <img src="./images/profile.jpg" class="card-image"/>
+        </div>
+        <div class="red-card-info">
+          <h3>Lorem Ipsum</h3>
+          <br/>
+          <h5>Lorem ipsum dolor sit</h5>
+          <p>Lorem ipsum | Lorem Ipsum | Lorem Ipsum | Lorem Ipsum</p>
+        </div>
+        <div class="card-social-icons">
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-facebook-square"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="yellow-card-background">
+          <img src="./images/profile.jpg" class="card-image"/>
+        </div>
+        <div class="yellow-card-info">
+          <h3>Lorem Ipsum</h3>
+          <br/>
+          <h5>Lorem ipsum dolor sit</h5>
+          <p>Lorem ipsum | Lorem Ipsum | Lorem Ipsum | Lorem Ipsum</p>
+        </div>
+        <div class="card-social-icons">
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-facebook-square"></i>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <div class="green-card-background">
+          <img src="./images/profile.jpg" class="card-image"/>
+        </div>
+        <div class="green-card-info">
+          <h3>Lorem Ipsum</h3>
+          <br/>
+          <h5>Lorem ipsum dolor sit</h5>
+          <p>Lorem ipsum | Lorem Ipsum | Lorem Ipsum | Lorem Ipsum</p>
+        </div>
+        <div class="card-social-icons">
+          <i class="fab fa-instagram"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-facebook-square"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     </>
   );
 };
